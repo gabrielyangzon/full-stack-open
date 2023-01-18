4fa235d6775efff7841adc38db0e27c7b4a8944b
Gabriel Lorenz Yangzon <gy04365249@shi-g.com>
2023-01-19 08:41:42 +0900

    added code for exercise 2.11

--- part2/phonebook/README.md
+++ part2/phonebook/README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+json-server --port 3001 --watch db.json
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,29 @@
-import React , {useState} from 'react'
+import React , {useEffect, useState} from 'react'
+import axios from 'axios'
 
 import Input from './components/Input'
 import Title from './components/Title'
 
 
 function App() {
-
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const[persons,setPersons] = useState([])
 
   const [newName, setNewName] = useState({id : "" ,name:"" , number:""})
   const [searchField, setSearchField] = useState("")
   const [filteredPersons,setFilteredPesons] = useState([...persons])
   const [error , setError] = useState("")
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons")
+      .then(response => {
+          console.log(response.data)
+          setPersons(response.data)
+          setFilteredPesons(response.data)
+      })
+      .catch(error => console.log(error))
+  },[])
+
 
   /// when user click add button
   function addPersonHandler(event){
